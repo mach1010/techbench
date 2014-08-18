ccbbae9219d59b04a97897cb6aebfb41f6f44256
mach1010 <jetstream1010@gmail.com>
2014-08-17 21:48:12 -0500

    added simple search and a couple adjustments to appearance

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,11 @@
 class Customer < ActiveRecord::Base
   validates :fname, :lname, presence: true
+  
+  def self.search(search)
+    if search
+      where("fname LIKE ? OR lname LIKE ? OR company_name LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%")
+    else
+      all
+    end
+  end
 end
